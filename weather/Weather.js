e6452b00c20ede239e9f5437c534a70d96fec112
T0R0NT0T0KY0 <Robbin-the-bobbin@yandex.ru>
2021-06-07 10:32:13 +0300

    resolve problem with Weather.js

--- weather/Weather.js
+++ weather/Weather.js
@@ -1,8 +1,7 @@
 const {Telegraf, Composer, Scenes, Markup, session} = require('telegraf');
 const OpenWeatherMapHelper = require("openweathermap-node");
-require('dotenv').config();
-const translateText = require("./Translate");
 const WEATHER_TOKEN = process.env.WEATHER_KEY;
+const translateText = require("./Translate.js");
 
 
 
@@ -24,24 +23,24 @@ startWizard.on('text', async ctx => {
 
 const titleStep = new Composer();
 titleStep.on('text', async (ctx) => {
-	const currCity = ctx.message.text
+	const currCity = ctx.message.text;
 	
 	if (currCity === "exit"){
-		await ctx.scene.leave()
+		await ctx.scene.leave();
 	}
-	//todo parse information
+	
 	translateText(currCity, 'en')
-			.then(result => helper.getCurrentWeatherByCityName(result,
-				async (err, currentWeather) => {
-					if (err) {
-						await ctx.reply("нет информации о таком городе, попробуйте другой");
-						await ctx.scene.reenter();
-					} else {
-						await ctx.reply(currentWeather);
-					}
-				}))
-			.catch(error => error.message);
+		.then(result => helper.getCurrentWeatherByCityName(result,
+			async (err, currentWeather) => {
+				if (err) {
+					await ctx.reply("нет информации о таком городе, попробуйте другой");
+					await ctx.scene.reenter();
+				} else {
+					await ctx.reply(currentWeather);
+				}
+			}))
+		.catch(error => error.message);
 	return ctx.scene.leave();
 });
 
-module.exports = {startWizard, titleStep}
+module.exports = {startWizard, titleStep};
