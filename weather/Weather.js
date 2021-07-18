68b1e96136845bc3d52a3f36cc36feee8cfda8bc
T0R0NT0T0KY0 <Robbin-the-bobbin@yandex.ru>
2021-07-18 15:51:16 +0300

    Добавлена проверка на свежесть погоды. Исправлены баги

--- weather/Weather.js
+++ weather/Weather.js
@@ -16,13 +16,9 @@ const helper = new OpenWeatherMapHelper(
 );
 
 const answer = async (inf) => {
-    let args = `Привет, сегодня ${week[new Date(inf.updatedAt).getDay()]}, в ${inf.city} `+
-     `${await translateText(inf.weather, 'ru')
-        .catch(()=>"")}\n`;
-    
-    // args += await translateText(inf.weather + "", 'ru')
-    //     .then(result => result + "\n")
-    //     .catch(() => inf.weather);
+    let args = `Привет, сегодня ${week[new Date(inf.updatedAt).getDay()]}, в ${inf.city} ` +
+        `${await translateText(inf.weather, 'ru')
+            .catch(() => "")}\n`;
     
     args += `Температура ${inf.temperature}°C\n`;
     
@@ -39,7 +35,7 @@ const getWeather = async (city) => {
         await helper.getCurrentWeatherByCityName(city,
             async (err, currentWeather) => {
                 if (err) {
-                    return null;
+                    return res(null);
                 }
                 return res({
                     id: currentWeather["id"],
@@ -70,13 +66,12 @@ firstScene.on('text', async ctx => {
 const lastScene = new Composer();
 
 const getWeatherFromDB = async (city) => {
-    const information = await fetchGetJson(`http://${host}:${port}/api/city/${city}`)
-        .catch(error => console.log(error));
-    try {
-        return  JSON.parse(information);
-    } catch (e) {
-        return  null;
-    }
+    const text = await fetchGetJson(`http://${host}:${port}/api/city/${city}`);
+    return text ? JSON.parse(text) : null;
+}
+
+function isFresh (res) {
+    return new Date(res.updatedAt) - new Date().valueOf() <= 500000;
 }
 
 lastScene.on('text', async (ctx) => {
@@ -84,18 +79,23 @@ lastScene.on('text', async (ctx) => {
     
     if (!(/[a-z]+/i.test(city))) { // если город не на английском
         city = await translateText(city, 'en');
-        console.log(city);
     }
     
     let res = await getWeatherFromDB(city);
     
+    if (res && !isFresh(res)) {
+        //todo update information
+        // res = fetchUpdate()
+    }
     if (!res) { // если нет в БД
         res = await getWeather(city);
         if (!res) {
+            ctx.reply("Нет информации о существовании такого города");
             await ctx.scene.reenter();
         }
-        console.log(await fetchPostJson(`http://${host}:${port}/api/city`, res)
-            .catch(e => e));
+        
+        await fetchPostJson(`http://${host}:${port}/api/city`, res)
+            .catch(e => e);
         // ложим в бд
     }
     
